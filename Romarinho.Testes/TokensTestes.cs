ebe411fb5071e1d42cac948dd89d02af34e695f2
Mauricio Ribeiro <mauricio.ribeiro@fieldcorp.com.br>
2022-07-11 12:17:28 -0300

    test: token testes

---
+++ Romarinho.Testes/TokensTestes.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using Romarinho.Core;
+using Romarinho.Domain.Model;
+using Romarinho.Infra;
+
+namespace Romarinho.Testes;
+
+public class TokenTestes
+{
+    [Theory]
+    [InlineData("38477992827", "adG/EqWd3VE+KnSqOvoSdMJbKh8f9utRmCeN2L1L3qCkzl+L7ovnEfTK5CyYPT5w")]
+    public async System.Threading.Tasks.Task ReverterTokenTeste(string cpf, string token)
+    {
+        var tokens = new Tokens();
+        token = tokens.Base64(token);
+        var cpfRetornado = tokens.ReverterToken(token);
+        var cpfEsperado = cpf;
+
+        Assert.Equal(cpfEsperado, cpfRetornado);
+    }
+}
