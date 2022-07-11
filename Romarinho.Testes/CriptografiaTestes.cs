5d5fb188b7e48b66169e605330d301feafa32bd8
Mauricio Ribeiro <mauricio.ribeiro@fieldcorp.com.br>
2022-07-11 12:02:07 -0300

    test: criptografia testes

---
+++ Romarinho.Testes/CriptografiaTestes.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using Romarinho.Core;
+using Romarinho.Domain.Model;
+using Romarinho.Infra;
+
+namespace Romarinho.Testes;
+
+public class CriptografiaTestes
+{
+    [Theory]
+    [InlineData("romarinho", "mPVCMXB4qcKzXFjiR3mmRQ==")]
+    [InlineData("Romarinho", "I2iY0ESssCuWpA3xwDhP7w==")]
+    public async System.Threading.Tasks.Task EncryptTeste(string romarinho, string romarinhoCriptografado)
+    {
+        var romarinhoRetornado = Criptografia.Encrypt(romarinho);
+        var romarinhoEsperado = romarinhoCriptografado;
+
+        Assert.Equal(romarinhoEsperado, romarinhoRetornado);
+    }
+
+    [Theory]
+    [InlineData("romarinho", "mPVCMXB4qcKzXFjiR3mmRQ==")]
+    [InlineData("Romarinho", "I2iY0ESssCuWpA3xwDhP7w==")]
+    public async System.Threading.Tasks.Task DecryptTeste(string romarinho, string romarinhoCriptografado)
+    {
+        var romarinhoRetornado = Criptografia.Decrypt(romarinhoCriptografado);
+        var romarinhoEsperado = romarinho;
+
+        Assert.Equal(romarinhoEsperado, romarinhoRetornado);
+    }
+}
